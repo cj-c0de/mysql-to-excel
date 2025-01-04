2923e027d1276c536152e057fbf1d64d3d64da28
dastgerdidev <dastgerdi.dev@gmail.com>
2025-01-04 22:55:36 +0330

    Convert whole db if table is None!

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class MySQLConfig:
     password: str
     database: str
 
-def convert_to_excel(config: MySQLConfig, table: str, output_file: str = None, columns: list = None):
+def convert_to_excel(config: MySQLConfig, table: str = None, output_file: str = None, columns: list = None):
     # Connect to MySQL database
     conn = mysql.connector.connect(
         host=config.host,
@@ -18,21 +18,27 @@ def convert_to_excel(config: MySQLConfig, table: str, output_file: str = None, c
         database=config.database
     )
 
-    # Fetch data from table
-    if columns:
-        columns_str = ', '.join(columns)
-    else:
-        columns_str = '*'
-    query = f'SELECT {columns_str} FROM {table}'
-    data = pd.read_sql(query, conn)
-
     # Determine output file name
     if not output_file:
-        output_file = f'{table}.xlsx'
+        output_file = f'{table if table else config.database}.xlsx'
 
-    # Save data to Excel
+    # Create Excel writer
     with pd.ExcelWriter(output_file) as writer:
-        data.to_excel(writer, index=False)
-
+        if table:
+            # Fetch data from specified table
+            columns_str = ', '.join(columns) if columns else '*'
+            query = f'SELECT {columns_str} FROM {table}'
+            data = pd.read_sql(query, conn)
+            data.to_excel(writer, index=False)
+        else:
+            # Fetch data from all tables
+            cursor = conn.cursor()
+            cursor.execute("SHOW TABLES")
+            tables = cursor.fetchall()
+            for (tbl_name,) in tables:
+                query = f'SELECT * FROM {tbl_name}'
+                data = pd.read_sql(query, conn)
+                data.to_excel(writer, sheet_name=tbl_name, index=False)
+    
     # Close the connection
     conn.close()
