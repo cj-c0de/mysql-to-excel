5f7826b2065487f4535ab73a5e887a19a374ad48
dastgerdidev <dastgerdi.dev@gmail.com>
2025-01-04 22:28:06 +0330

    Save data to excel

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ conn = mysql.connector.connect(
 table = 'table'
 
 # Fetch data from table
-query = f"SELECT * FROM {table}"
+query = f'SELECT * FROM {table}'
 data = pd.read_sql(query, conn)
+
+# Save data to Excel
+with pd.ExcelWriter(f'{table}.xlsx') as writer:
+    data.to_excel(writer, index=False)
 
 # Close the connection
 conn.close()
