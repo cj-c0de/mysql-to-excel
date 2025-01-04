4ed6e8f330b1e25f11316f4f32c042b5cd308007
dastgerdidev <dastgerdi.dev@gmail.com>
2025-01-04 22:41:13 +0330

    Turn to a function!

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import mysql.connector
 import pandas as pd
 
-# Connect to MySQL database
-conn = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='',
-    database='db'
-)
-table = 'table'
+def convert_to_excel(host, user, password, database, table, output_file):
+    # Connect to MySQL database
+    conn = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
 
-# Fetch data from table
-query = f'SELECT * FROM {table}'
-data = pd.read_sql(query, conn)
+    # Fetch data from table
+    query = f'SELECT * FROM {table}'
+    data = pd.read_sql(query, conn)
 
-# Save data to Excel
-with pd.ExcelWriter(f'{table}.xlsx') as writer:
-    data.to_excel(writer, index=False)
+    # Save data to Excel
+    with pd.ExcelWriter(output_file) as writer:
+        data.to_excel(writer, index=False)
 
-# Close the connection
-conn.close()
+    # Close the connection
+    conn.close()
